56d24588d5dbd8225c5cb1c3dfb1c1130f842645
superezz <248160976+superezz@users.noreply.github.com>
2026-09-11 19:48:13 +0530

    feat: implement authentication flow with Firebase, including login route and user creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
-
+import { signInWithPopup } from "firebase/auth";
+import React from "react";
+import { auth, googleProvider } from "../utils/firebase";
+import api from "../utils/axios";
 
 function App() {
+  const handleLogin = async (token) => {
+    try {
+      const { data } = await api.post("/api/auth/login", { token });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const googleLogin = async () => {
+    const data = await signInWithPopup(auth, googleProvider);
+    const token = await data.user.getIdToken();
+
+    console.log(token);
+    await handleLogin(token);
+    console.log(data);
+  };
+
   return (
-    <div>Hello, World!</div>
-  )
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <button className="w-50 h-24 bg-white" onClick={googleLogin}>
+        continue with google
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "koggent-cd5cc.firebaseapp.com",
+  projectId: "koggent-cd5cc",
+  storageBucket: "koggent-cd5cc.firebasestorage.app",
+  messagingSenderId: "1021848054049",
+  appId: "1:1021848054049:web:1d86d29abeba7fa31d7660",
+  measurementId: "G-RWWQG2WNHZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
