842ce3e7aebbcc89c6a94745cbbb781a5938570a
superezz <248160976+superezz@users.noreply.github.com>
2026-09-12 20:43:29 +0530

    feat(agent): implement agent microservice with multiple agents and routing

---
+++ backend/services/agent/graph/router.js
@@ -0,0 +1,72 @@
+import { getModel } from "../config/llmModels.js";
+
+export const router = async (state) => {
+  const llm = getModel("router");
+
+  const prompt = `You are an agent router.
+
+Available agents:
+
+- chat
+- search
+- coding
+- pdf
+- ppt
+- vision
+
+Rules:
+
+chat:
+General conversation,
+explanations,
+learning,
+questions.
+
+search:
+Current events,
+latest information,
+news,
+recent developments,
+internet lookup.
+
+coding:
+Generate code,
+debug code,
+build projects,
+architecture,
+API design.
+
+pdf:
+Questions about generate PDFs
+or document context.
+
+ppt:
+Questions about generate ppts
+or ppt context.
+
+vision:
+Generate image,
+create image
+
+Return ONLY one word:
+
+chat
+search
+coding
+pdf
+ppt
+vision
+
+User Query:
+
+${state.prompt}
+`;
+
+  const response = await llm.invoke(prompt);
+  console.log(response);
+
+  return {
+    ...state,
+    agent: response.content.trim().toLowerCase(),
+  };
+};
--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDb} from "./config/db.js";
+import { router } from "./graph/router.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+app.use("/", router);
 
 
 app.get("/", (req, res) => {
